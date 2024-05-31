413522b030514c396418096b4f79119df2175058
YvesCB <y.bruckmayer@gmail.com>
2024-05-31 09:57:53 +0200

    front server

--- front-server/src/main.rs
+++ front-server/src/main.rs
@@ -1,3 +1,45 @@
+use web_sys::console;
+use yew::prelude::*;
+
+#[derive(Properties, PartialEq)]
+pub struct Props {
+    pub on_name_entry: Callback<String>,
+}
+
+#[function_component]
+fn HelloWorld(props: &Props) -> Html {
+    props.on_name_entry.emit(String::from("Bob"));
+
+    html! {
+        <p>{ "Hello" }</p>
+    }
+}
+
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    let on_name_entry: Callback<String> = Callback::from(move |name: String| {
+        let greeting = format!("Hello {}", &name);
+        console::log_1(&greeting.into());
+    });
+
+    html! {
+        <div>
+            <HelloWorld {on_name_entry} />
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
